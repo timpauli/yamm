91d4c8cff5a089781a2d3b86551bfd2c682e9ac1
inkeye <tim.pauli@posteo.de>
2017-11-24 16:42:00 +0100

    first wave of tests

--- tests/chain_test.py
+++ tests/chain_test.py
@@ -5,8 +5,29 @@ from yamm import Chain
 
 class ChainTest(TestCase):
 
-    def test(self):
-        Chain()
+    def test_from_data1(self):
+        test = Chain.test_from_data([1, 2, 1, 1])
+        check = {(1,): {1: 1, 2: 1},
+                 (2,): {1: 1}}
+
+        self.assertEqual(test, check)
+
+    def test_from_data2(self):
+        test = Chain.test_from_data([1, 2, 1, 1], 2)
+        check = {(1, 2): {1: 1},
+                 (2, 1): {1: 1}}
+
+        self.assertEqual(test, check)
+
+    def test_from_matrix(self):
+        pass
+
+    def test_step(self):
+        chain = {(1,): {1: 1, 2: 1},
+                 (2,): {1: 1}}
+        test = chain.step(2)
+        check = 1
+        self.assertEqual(test, check)
 
 
 if __name__ == '__main__':
